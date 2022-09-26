e801aa8988477ad2c4e67e132515a09619dac203
Anshika Gautam <anshikagautam130@gmail.com>
2022-09-26 21:21:11 +0530

    Middlewares

---
+++ server/middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyJWT = async (req, res, next) => {
+  const token = req.headers["x-access-token"]?.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err)
+        return res.json({
+          isLoggedIn: false,
+          message: "Failed to Authenticate",
+        });
+
+      req.user = {};
+      req.user.id = decoded.id;
+      req.user.name = decoded.name
+
+      next();
+    });
+  } else {
+    res.json({ message: "Incorrect Token Given", isLoggedIn: false });
+  }
+};
