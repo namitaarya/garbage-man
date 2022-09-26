f183f92378e34baa94eec188907819a8ba83c250
Namita Arya <namitaarya2002@gmail.com>
2022-09-26 23:26:44 +0530

    adding location on contact us

--- client/src/Pages/ContactUs/contactus.js
+++ client/src/Pages/ContactUs/contactus.js
@@ -46,7 +46,17 @@ export default function ContactUs(){
           </form>
         </div>
         <div className="right">
-          <img src="/Asset/contact.jpg" alt="" />
+        <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.8439304759995!2d77.2292465141924!3d28.66439138939098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd0683919c3b%3A0xf5fc331b74c2b9e2!2sIndira%20Gandhi%20Delhi%20Technical%20University%20for%20Women!5e0!3m2!1sen!2sin!4v1664214789224!5m2!1sen!2sin"
+              width="600"
+              height="450"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            />
+          {/* <img src="/Asset/contact.jpg" alt="" /> */}
         </div>
       </div>
     </div>
